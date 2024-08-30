4b2ee00e853f26b5029bf88b92042310dd033f8d
sheshukurnool <sheshukurnool@gmail.com>
2024-08-30 17:47:33 +0530

    issues resolved in logout and now user cannot go back to a previous page after logging out

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,19 +8,36 @@ const Header: React.FC = () => {
   const pathname = usePathname();
   const { isLoggedIn, logout } = useAuth();
   const [hideLinks, setHideLinks] = useState<boolean>(false);
+  const router = useRouter();
 
-useEffect(() => {
-  // List of URLs where links should be hidden
-  const urlsToHideLinks = ["/verifycode", "/reset-password", "/profile-schema"];
+  useEffect(() => {
+    // List of URLs where links should be hidden
+    const urlsToHideLinks = ["/verifycode", "/reset-password", "/profile-schema"];
 
-  // Determine if the links should be hidden based on the pathname
-  setHideLinks(urlsToHideLinks.includes(pathname));
-}, [pathname]);
+    // Determine if the links should be hidden based on the pathname
+    setHideLinks(urlsToHideLinks.includes(pathname));
+  }, [pathname]);
 
   const handleLogout = () => {
-    router.push("/login");
     logout();
+    // Clear history and redirect to login
+    window.history.pushState(null, null, '/login');
+    router.push("/login");
   };
+
+  useEffect(() => {
+    const handlePopState = (event: PopStateEvent) => {
+      if (!isLoggedIn) {
+        router.replace('/login');  // Redirect to login if not logged in
+      }
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [isLoggedIn, router]);
 
   return (
     <header className="bg-green-400 shadow">
